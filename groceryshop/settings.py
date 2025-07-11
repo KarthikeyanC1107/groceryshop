3c98e4a3c822808741dcf7c58be8ae497fd60706
KarthikeyanC1107 <karthikeyankn1107@gmail.com>
2025-07-11 05:45:56 +0530

    Restrict allowed hosts to Render domain

--- groceryshop/settings.py
+++ groceryshop/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-&d+%_xedg)w(or09!wwqk&m&%*&#4@7oa%org=a)#pnyf&7xfb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'chandrasekar-stores.onrender.com',  # your Render URL
-    'localhost',
-    '127.0.0.1',]
+ALLOWED_HOSTS = ['chandrasekar-stores.onrender.com']
 
 
 # Application definition
